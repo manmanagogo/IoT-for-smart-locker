3d1ac13cfd555e2269b24596f1d9306d08d8bfce
AstroDOG3 <77763809+AstroDOG3@users.noreply.github.com>
2024-08-19 14:43:31 +0700

    frontend

---
+++ src/pages/LoginPage.jsx
@@ -0,0 +1,15 @@
+function LoginPage() {
+    return (
+        <div className="flex items-center justify-center flex-col">
+            <h1 className="text-3xl text-center m-5">KU-Login</h1>
+            <div>
+                <h1 className= "text-2xl mb-5">E-mail :</h1>
+                <input className="Input box border border-gray-300 p-2 rounded-xl " type="text" />
+
+
+            </div>
+        </div>
+
+    );
+}
+export default LoginPage;
